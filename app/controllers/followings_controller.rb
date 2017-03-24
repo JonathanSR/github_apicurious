be242371b5953bc7730db4f8c0cd8f201533703c
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-03-24 08:31:18 -0600

    adds before action authorize method

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,4 +1,5 @@
 class FollowingsController < ApplicationController
+before_action :authorize!
 
   def index
     @followings = Following.find_following_info(current_user)
--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,4 +1,5 @@
 class ReposController < ApplicationController
+before_action :authorize!
   
   def index
     @repos = Repo.find_repo_info(current_user)
